88568f9c4ab9999667f66ac4886b62d300f886c6
karupanerura <karupa@cpan.org>
2023-11-25 17:47:49 +0900

    split db to server2

--- db_setup.sh
+++ db_setup.sh
@@ -1,3 +1,5 @@
- cat webapp/sql/initdb.d/01_reset_database.sql | sudo mysql isupipe
- cat webapp/sql/initdb.d/10_schema.sql | sudo mysql isupipe
- sh webapp/sql/init.sh
+set -uex
+cat webapp/sql/initdb.d/00_create_database.sql | sudo mysql -uroot isupipe
+cat webapp/sql/initdb.d/01_reset_database.sql | sudo mysql -uroot isupipe
+cat webapp/sql/initdb.d/10_schema.sql | sudo mysql -uroot isupipe
+sh webapp/sql/init.sh
--- sql/initdb.d/00_create_database.sql
+++ sql/initdb.d/00_create_database.sql
@@ -3,9 +3,11 @@ CREATE DATABASE IF NOT EXISTS `isupipe`;
 DROP USER IF EXISTS `isucon`@`%`;
 CREATE USER isucon IDENTIFIED BY 'isucon';
 GRANT ALL PRIVILEGES ON isupipe.* TO 'isucon'@'%';
+GRANT ALL PRIVILEGES ON isupipe.* TO 'isucon'@'localhost';
 
 CREATE DATABASE IF NOT EXISTS `isudns`;
 
 DROP USER IF EXISTS `isudns`@`%`;
 CREATE USER isudns IDENTIFIED BY 'isudns';
 GRANT ALL PRIVILEGES ON isudns.* TO 'isudns'@'%';
+GRANT ALL PRIVILEGES ON isudns.* TO 'isudns'@'localhost';
